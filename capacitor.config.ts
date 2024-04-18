c404fa46d47f9265799a8a6d4453528c3b23b1a8
Ollie Beenham <ollie@beenhamow.co.uk>
2024-04-18 23:13:44 +0100

    Initial ionic commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "io.ionic.starter",
+    appName: "edisons-sleep-app",
+    webDir: "dist",
+};
+
+export default config;
